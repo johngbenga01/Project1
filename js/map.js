e9592eef9ea239716f0658343ac5686ebdff9c8c
johngbenga01 <101526119+johngbenga01@users.noreply.github.com>
2022-10-03 00:58:56 +0200

    update

--- js/map.js
+++ js/map.js
@@ -1,12 +1,95 @@
 // Initialise map
-var map = L.map('map',{
+var map = L.map('map', {
+    
+    zoomControl: false,
 
- 
-}).setView([6.52, 3.53 ], 10);
 
+
+}).setView([6.52, 3.53], 10);
+
+
+
+// custom zoom bar control that includes a Zoom Home function
+L.Control.zoomHome = L.Control.extend({
+    options: {
+        position: 'topleft',
+        zoomInText: '+',
+        zoomInTitle: 'Zoom in',
+        zoomOutText: '-',
+        zoomOutTitle: 'Zoom out',
+        zoomHomeText: '<i class="fa fa-home" style="line-height:1.65;"></i>',
+        zoomHomeTitle: 'Zoom home'
+    },
+
+    onAdd: function(map) {
+        var controlName = 'gin-control-zoom',
+            container = L.DomUtil.create('div', controlName + ' leaflet-bar'),
+            options = this.options;
+
+        this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
+            controlName + '-in', container, this._zoomIn);
+        this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
+            controlName + '-home', container, this._zoomHome);
+        this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
+            controlName + '-out', container, this._zoomOut);
+
+        this._updateDisabled();
+        map.on('zoomend zoomlevelschange', this._updateDisabled, this);
+
+        return container;
+    },
+
+    onRemove: function(map) {
+        map.off('zoomend zoomlevelschange', this._updateDisabled, this);
+    },
+
+    _zoomIn: function(e) {
+        this._map.zoomIn(e.shiftKey ? 3 : 1);
+    },
+
+    _zoomOut: function(e) {
+        this._map.zoomOut(e.shiftKey ? 3 : 1);
+    },
+
+    _zoomHome: function(e) {
+        map.setView([6.52, 3.53], 10);
+    },
+
+    _createButton: function(html, title, className, container, fn) {
+        var link = L.DomUtil.create('a', className, container);
+        link.innerHTML = html;
+        link.href = '#';
+        link.title = title;
+
+        L.DomEvent.on(link, 'mousedown dblclick', L.DomEvent.stopPropagation)
+            .on(link, 'click', L.DomEvent.stop)
+            .on(link, 'click', fn, this)
+            .on(link, 'click', this._refocusOnMap, this);
+
+        return link;
+    },
+
+    _updateDisabled: function() {
+        var map = this._map,
+            className = 'leaflet-disabled';
+
+        L.DomUtil.removeClass(this._zoomInButton, className);
+        L.DomUtil.removeClass(this._zoomOutButton, className);
+
+        if (map._zoom === map.getMinZoom()) {
+            L.DomUtil.addClass(this._zoomOutButton, className);
+        }
+        if (map._zoom === map.getMaxZoom()) {
+            L.DomUtil.addClass(this._zoomInButton, className);
+        }
+    }
+});
+
+var zoomHome = new L.Control.zoomHome();
+zoomHome.addTo(map);
 //Test
 
-L.DomUtil.setOpacity(map.zoomControl.getContainer(), 100); // set opacity of zoom buttons
+//L.DomUtil.setOpacity(map.zoomControl.getContainer(), 100); // set opacity of zoom buttons
 
 // Add Esri tile layer
 //var esri = L.esri.basemapLayer('Topographic').addTo(map)
@@ -15,7 +98,7 @@ L.DomUtil.setOpacity(map.zoomControl.getContainer(), 100); // set opacity of zoo
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: "ESA, Open Street Map"
-   
+
 }).addTo(map);
 
 var googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}', {
@@ -25,10 +108,10 @@ var googleStreets = L.tileLayer('http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={
 });
 
 var googleTraffic = L.tileLayer('https://{s}.google.com/vt/lyrs=m@221097413,traffic&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        minZoom: 2,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    });
+    maxZoom: 20,
+    minZoom: 2,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+});
 
 var googleHybrid = L.tileLayer('http://{s}.google.com/vt?lyrs=s,h&x={x}&y={y}&z={z}', {
     maxZoom: 20,
@@ -135,7 +218,7 @@ var lagosStyle = {
 var lagosLayer = L.geoJson(lagos, {
     style: lagosStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -153,7 +236,7 @@ var lagosLayer = L.geoJson(lagos, {
 var waterLayer = L.geoJson(water, {
     style: waterStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -172,7 +255,7 @@ var waterLayer = L.geoJson(water, {
 var grasslandLayer = L.geoJson(grassland, {
     style: grasslandStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -191,7 +274,7 @@ var grasslandLayer = L.geoJson(grassland, {
 var builtupLayer = L.geoJson(builtup, {
     style: builtupStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -210,7 +293,7 @@ var builtupLayer = L.geoJson(builtup, {
 var croplandLayer = L.geoJson(cropland, {
     style: croplandStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -229,7 +312,7 @@ var croplandLayer = L.geoJson(cropland, {
 var treesLayer = L.geoJson(trees, {
     style: treesStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -248,7 +331,7 @@ var treesLayer = L.geoJson(trees, {
 var shrublandLayer = L.geoJson(shrubland, {
     style: shrublandStyle,
     onEachFeature: function(feature, layer) {
-        area = (turf.area(feature)/1000000).toFixed(3)
+        area = (turf.area(feature) / 1000000).toFixed(3)
         center_lng = turf.center(feature).geometry.coordinates[0].toFixed(2)
         center_lat = turf.center(feature).geometry.coordinates[1].toFixed(2)
 
@@ -273,8 +356,16 @@ var waterBodiesWMS = L.tileLayer.wms("http://localhost:8080/geoserver/geospatial
     format: 'image/png',
     transparent: true,
     attribution: ""
-})//.addTo(map)
+}) //.addTo(map)
 
+// Add measure control
+
+L.control.measure({ 
+    primaryLengthUnit: 'kilometers', 
+    secondaryLengthUnit: 'meters',
+    primaryAreaUnit: 'sqmeters', 
+    secondaryAreaUnit: undefined 
+}).addTo(map);
 
 // Basemaps
 var baseLayers = {
@@ -299,12 +390,28 @@ var overlays = {
     "Water Tile": waterBodiesWMS,
 };
 
+
+// Full screen map view
+
+// var mapId = document.getElementById("map");
+// function fullScreenView(){
+//     mapId.requestFullscreen();
+// }
+
+
 // Add layer control to map
-L.control.layers(baseLayers, overlays,{ collapsed: true }).addTo(map);
+L.control.layers(baseLayers, overlays, { collapsed: true }).addTo(map);
 
+// Map print
 
+$(`.print-map`).click(function () {
+
+    window.print();
+});
 // Add leaflet print control
 L.control.browserPrint({ position: 'topleft' }).addTo(map);
+
+
 
 // Add measure control
 // var map = L.map('map', {
